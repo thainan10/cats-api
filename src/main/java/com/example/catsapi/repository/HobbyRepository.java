24ede586cb137b9e86190790cd7a6d3bd901193e
Thainan Matias <thainan@dynavideo.com.br>
2018-08-24 08:01:04 -0300

    Enable cross origin requests to hobbies resource

--- src/main/java/com/example/catsapi/repository/HobbyRepository.java
+++ src/main/java/com/example/catsapi/repository/HobbyRepository.java
@@ -3,7 +3,9 @@ package com.example.catsapi.repository;
 import com.example.catsapi.domain.Hobby;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "*")
 public interface HobbyRepository extends PagingAndSortingRepository<Hobby, Long> {
 }
