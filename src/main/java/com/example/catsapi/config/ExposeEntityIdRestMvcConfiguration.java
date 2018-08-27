724790721339b66c7fc6d89e62aacf1616403f50
Thainan Matias <thainan@dynavideo.com.br>
2018-08-27 15:31:12 -0300

    Update hobby requests to return the ids

---
+++ src/main/java/com/example/catsapi/config/ExposeEntityIdRestMvcConfiguration.java
@@ -0,0 +1,15 @@
+package com.example.catsapi.config;
+
+import com.example.catsapi.domain.Hobby;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExposeEntityIdRestMvcConfiguration extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Hobby.class);
+    }
+}
